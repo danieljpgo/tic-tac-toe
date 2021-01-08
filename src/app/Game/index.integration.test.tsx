1f2aa77207e3c9508f11012a49236514c289cd20
Daniel Jorge <djpgonzaga@gmail.com>
2021-01-08 02:02:59 -0300

    test: add integration test - no more moves may be played after game is over

--- src/app/Game/index.integration.test.tsx
+++ src/app/Game/index.integration.test.tsx
@@ -52,3 +52,37 @@ test('can play a game of tic tac toe', () => {
   userEvent.click(p9);
   expect(p9).toHaveTextContent(/x/i);
 });
+
+test('no more moves may be played after game is over', () => {
+  render(<Game />);
+
+  const [
+    p1, p2, p3,
+    p4, p5, p6,
+  ] = Array.from(screen.queryAllByRole('button'));
+
+  expect(screen.getByText(/next player: x/i)).toBeInTheDocument();
+  userEvent.click(p1);
+  expect(p1).toHaveTextContent(/x/i);
+
+  expect(screen.getByText(/next player: o/i)).toBeInTheDocument();
+  userEvent.click(p4);
+  expect(p4).toHaveTextContent(/o/i);
+
+  expect(screen.getByText(/next player: x/i)).toBeInTheDocument();
+  userEvent.click(p2);
+  expect(p2).toHaveTextContent(/x/i);
+
+  expect(screen.getByText(/next player: o/i)).toBeInTheDocument();
+  userEvent.click(p5);
+  expect(p5).toHaveTextContent(/o/i);
+
+  expect(screen.getByText(/next player: x/i)).toBeInTheDocument();
+  userEvent.click(p3);
+  expect(p3).toHaveTextContent(/x/i);
+
+  expect(screen.getByText(/winner: x/i)).toBeInTheDocument();
+  userEvent.click(p6);
+  expect(p6).toHaveTextContent('');
+});
+
