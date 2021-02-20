b2231ac3c49de5d06a4b3ce3df56cb06978cd4f5
Daniel Jorge <djpgonzaga@gmail.com>
2021-02-20 15:23:26 -0300

    fix: layout shift when make a new step on mobile

--- src/app/pages/Game/Container/index.tsx
+++ src/app/pages/Game/Container/index.tsx
@@ -6,7 +6,7 @@ const Container = (props: Props) => {
   const { children } = props;
 
   return (
-    <div className="grid gap-6 px-8 overflow-y-auto sm:gap-4 sm:overflow-y-visible sm:px-0 sm:grid-cols-2 sm:auto-cols-min">
+    <div className="grid gap-6 px-8 overflow-y-hidden sm:gap-4 sm:overflow-y-visible sm:px-0 sm:grid-cols-2 sm:auto-cols-min">
       {children}
     </div>
   );
