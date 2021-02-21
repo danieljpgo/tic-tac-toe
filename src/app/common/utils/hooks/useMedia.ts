dc44a03d5d63866f164841f531543a4bec9d4ee4
Daniel Jorge <djpgonzaga@gmail.com>
2021-02-21 03:41:54 -0300

    feat: add useMediaQuery hook

---
+++ src/app/common/utils/hooks/useMedia.ts
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+const breakpoints = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+};
+
+type Breakpoints = keyof typeof breakpoints;
+
+export const useMediaQuery = (query: Breakpoints) => {
+  const [matches, setMatches] = React.useState(false);
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: ${breakpoints[query]})`);
+    setMatches(mediaQuery.matches);
+
+    mediaQuery.addEventListener('change', (event) => {
+      setMatches(event.matches);
+    });
+
+    return () => mediaQuery.removeEventListener('change', (event) => {
+      setMatches(event.matches);
+    });
+  }, []);
+  return matches;
+};
